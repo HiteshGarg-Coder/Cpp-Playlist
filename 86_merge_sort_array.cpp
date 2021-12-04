2ecec151357c244a3e8a3e84bc907afb204609db
hitesh <hitesh.11711637@lpu.in>
2021-12-04 19:57:23 +0530

    single array sorted in non-decreasing order

--- 86_merge_sort_array.cpp
+++ 86_merge_sort_array.cpp
@@ -1,10 +1,33 @@
-# Cpp-Playlist
-
 #include<bits/stdc++.h>
 using namespace std;
 
 class Leetcode {
     public:
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        //declare variables
+        int p1 = m-1, p2 = n-1, current = n+m-1;
+
+        // untill one of them becomes completely merged
+        while(p1 >= 0 && p2 >= 0) {
+            //larger element
+            if(nums1[p1] > nums2[p2]) {
+                nums1[current--] = nums1[p1--];
+            } else {
+                nums1[current--] = nums2[p2--];
+            }
+        }
+
+        //one of them is completely merged
+        while(p1 >= 0) {
+            nums1[current--] = nums1[p1--];
+        } 
+
+        while(p2 >= 0) {
+            nums1[current--] = nums2[p2--];
+        }
+    }
 
     void print(vector<int> &arr) {  
         cout << endl;
@@ -19,6 +42,9 @@ class Leetcode {
     }
 
     int solve(vector<int> &nums, int n, vector<int> &nums2, int m) {
+        print(nums);
+        merge(nums,n, nums2, m);
+        print(nums);
         return 0;
     }
 };
